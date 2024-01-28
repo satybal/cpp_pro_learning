4458202d81595d15878ec2ba251abd1f4c576832
satybal <satybal_tim@mail.ru>
2024-01-28 23:17:29 +0300

    ip address homework added

---
+++ 02_ipaddress/ip_filter.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string> split(const std::string &str, char d);
+
+void reverse_sort(std::vector<std::vector<std::string>>& pool);
+
+void print_ip(std::vector<std::string>& line);
+
+
---
+++ 02_ipaddress/main.cpp
@@ -0,0 +1,61 @@
+#include <fstream>
+#include "ip_filter.hpp"
+
+int main(int, char **) {
+
+    std::ifstream in_file {"ip_filter.tsv"};
+    if (!in_file.is_open()) {
+        std::cout << "Failed to open file ip_filter.tsv!" << std::endl;
+        return -1;
+    }
+
+    try
+    {
+        std::vector<std::vector<std::string>> ip_pool;
+
+        for(std::string line; std::getline(in_file, line);)
+        {
+            std::vector<std::string> v = split(line, '\t');
+            ip_pool.push_back(split(v.at(0), '.'));
+        }
+
+        // Reverse sort
+        reverse_sort(ip_pool);
+        
+        // Sorted output
+        for(auto& ip: ip_pool) {
+            print_ip(ip);
+        }
+
+        // Print where first number is 1
+        for (auto& ip: ip_pool) {
+            if (ip[0] == "1") 
+                print_ip(ip);
+        }
+
+        // Print where first is 46 and the second is 70
+        for (auto& ip: ip_pool) {
+            if (ip[0] == "46" && ip[1] == "70") 
+                print_ip(ip);
+        }
+
+        // Print if any is 46
+        for (auto& ip: ip_pool) {
+            bool flag {false};
+            for (auto& part: ip) {
+                if (part == "46") {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) 
+                print_ip(ip);
+        }
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
